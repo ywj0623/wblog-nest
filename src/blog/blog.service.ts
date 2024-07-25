8d43f9eab51fd8a9e44d4840c61dec37d9a042fb
Bûn <43789678+ywj0623@users.noreply.github.com>
2024-07-26 00:02:25 +0800

    feat: add blog service

---
+++ src/blog/blog.service.ts
@@ -0,0 +1,39 @@
+import { Injectable } from '@nestjs/common'
+import { Model } from 'mongoose'
+import { InjectModel } from '@nestjs/mongoose'
+import { Post } from './interfaces/post.interface'
+import { CreatePostDTO } from './dto/create-post.dto'
+
+@Injectable()
+export class BlogService {
+  constructor(@InjectModel('Post') private readonly postModel: Model<Post>) {}
+
+  async getPosts(): Promise<Post[]> {
+    const posts = await this.postModel.find().exec()
+    return posts
+  }
+
+  async getPost(postId: number): Promise<Post> {
+    const post = await this.postModel.findById(postId).exec()
+    return post
+  }
+
+  async addPost(createPostDTO: CreatePostDTO): Promise<Post> {
+    const newPost = await this.postModel(createPostDTO)
+    return newPost.save()
+  }
+
+  async editPost(postId: number, createPostDTO: CreatePostDTO): Promise<Post> {
+    const editedPost = await this.postModel.findByIdAndUpdate(
+      postId,
+      createPostDTO,
+      { new: true },
+    )
+    return editedPost
+  }
+
+  async deletePost(postId: number): Promise<Post> {
+    const deletedPost = this.postModel.findByIdAndDelete(postId)
+    return deletedPost
+  }
+}
