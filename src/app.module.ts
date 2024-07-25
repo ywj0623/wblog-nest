9c42e98edb4d18e3b70af8220dc77151c757139c
Bûn <43789678+ywj0623@users.noreply.github.com>
2024-07-26 00:04:16 +0800

    feat: update app module & controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,13 @@ import { Module } from '@nestjs/common'
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
 import { MongooseModule } from '@nestjs/mongoose'
+import { BlogModule } from './blog/blog.module'
 
 @Module({
-  imports: [MongooseModule.forRoot('mongodb://root:root@localhost:27017/')],
+  imports: [
+    MongooseModule.forRoot('mongodb://root:root@localhost:27017/'),
+    BlogModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
