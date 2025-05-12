ace9b241d04c0409eb20cf360dda2d5cf9ca349a
Bûn <43789678+ywj0623@users.noreply.github.com>
2025-05-12 22:36:22 +0800

    feat: rename bootstrap function to wBlog

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,10 @@ import { NestFactory } from '@nestjs/core'
 import { AppModule } from 'src/modules/app.module'
 import { ValidationPipe } from '@nestjs/common'
 
-async function bootstrap() {
+async function wBlog() {
   const app = await NestFactory.create(AppModule)
   app.useGlobalPipes(new ValidationPipe())
   app.enableCors()
   await app.listen(8000)
 }
-bootstrap()
+wBlog()
