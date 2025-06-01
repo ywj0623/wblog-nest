083a25a66ebe7837d0541c9be67ff87edfeaf066
Bûn <43789678+ywj0623@users.noreply.github.com>
2025-06-01 19:29:11 +0800

    feat: 重構 TokenService，整合 JwtUtil 以簡化令牌生成邏輯，並更新相關方法簽名

--- src/services/token/token.service.ts
+++ src/services/token/token.service.ts
@@ -1,37 +1,55 @@
 import { BadRequestException, Injectable } from '@nestjs/common'
 import { Token, TokenDocument } from 'src/entity/token.entity'
-import { Model, Schema as MongooseSchema } from 'mongoose'
+import { Model } from 'mongoose'
 import { InjectModel } from '@nestjs/mongoose'
+import { JwtUtil } from 'src/common/utils/jwt.util'
+import { User } from 'src/entity/user.entity'
+import { UserPayloadDTO } from 'src/resolvers/auth/dto/user.dto'
 
 @Injectable()
 export class TokenService {
-  constructor(@InjectModel(Token.name) private readonly tokenModel: Model<TokenDocument>) {}
+  constructor(
+    private jwtUtil: JwtUtil,
+    @InjectModel(Token.name) private readonly tokenModel: Model<TokenDocument>,
+  ) {}
 
-  async createToken(userId: MongooseSchema.Types.ObjectId, refresh_token: string, access_token: string): Promise<Token> {
-    if (!userId || !refresh_token || !access_token) {
+  async createToken(user: User): Promise<Token> {
+    if (!user._id) {
       throw new BadRequestException('User ID, refresh token, and access token are required')
     }
+    const refresh_token = this.jwtUtil.generateRefreshToken(user)
+    const access_token = this.jwtUtil.generateAccessToken(user)
 
-    return this.tokenModel.create({
-      user_id: userId,
+    const result = await this.tokenModel.create({
+      user_id: user._id,
       refresh_token,
       access_token,
     })
+
+    return result
   }
 
-  async refreshToken(userId: MongooseSchema.Types.ObjectId, refresh_token: string, access_token: string): Promise<Token> {
-    if (!userId || !refresh_token || !access_token) {
+  async refreshToken(user: UserPayloadDTO): Promise<Token> {
+    if (!user._id) {
       throw new BadRequestException('User ID, refresh token, and access token are required')
     }
 
-    const existingTokenUser = await this.tokenModel.findOne({ user_id: userId })
+    const existingTokenUser = await this.tokenModel.findOne({ user_id: user._id })
 
     if (existingTokenUser) {
-      existingTokenUser.refresh_token = refresh_token
-      existingTokenUser.access_token = access_token
+      existingTokenUser.refresh_token = this.jwtUtil.generateRefreshToken(user)
+      existingTokenUser.access_token = this.jwtUtil.generateAccessToken(user)
       return existingTokenUser.save()
     } else {
-      return this.createToken(userId, refresh_token, access_token)
+      return this.createToken(user)
     }
+  }
+
+  async clearToken(user: UserPayloadDTO): Promise<void> {
+    if (!user._id) {
+      throw new BadRequestException('User ID is required to clear token')
+    }
+
+    await this.tokenModel.deleteOne({ user_id: user._id }).exec()
   }
 }
